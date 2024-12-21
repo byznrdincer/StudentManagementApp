39161bc81c37d209fedd1535136af9ef15ecb42a
Beyzanur <beyzanurdincer502@gmail.com>
2024-12-21 21:57:46 +0300

    frontend için gerekli sınıflar olusturuluyor

--- src/main/java/com/lbs/Studentmanagementapp/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.lbs.Studentmanagementapp.service;
-
-
-public interface AuthService {
-    boolean validateStudent(int studentId, String studentName);
-    boolean validateTeacher(int teacherId, String teacherName);
-}
